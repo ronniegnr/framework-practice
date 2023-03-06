5665608d3942aa2833c3268e2fad5135c32e8998
ronniegnr <gnr.ronnie@gmail.com>
2023-03-06 21:27:43 +0600

    user registration implemented with database insert

--- rcommerce-express-ejs/src/controllers/userController.js
+++ rcommerce-express-ejs/src/controllers/userController.js
@@ -1,14 +1,53 @@
-const registerUser = (req, res) => {
-    res.status(200).json({message: 'Register User'})
-}
+const asyncHandler = require("express-async-handler");
+const bcrypt = require('bcryptjs')
+const User = require('../models/userModel')
 
-const loginUser = (req, res) => {
+const registerUser = asyncHandler(async (req, res) => {
+    const {name, email, password} = req.body
+
+    if (!name || !email || !password) {
+        res.status(400)
+        throw new Error('Please add all fields')
+    }
+
+    const userExists = await User.findOne({email})
+    if (userExists) {
+        res.status(400)
+        throw new Error('User already exists')
+    }
+
+    const salt = await bcrypt.genSalt()
+    const hashedPassword = await bcrypt.hash(password, salt)
+
+    const user = await User.create({
+        name,
+        email,
+        password: hashedPassword
+    })
+
+    if (user) {
+        res.status(401).json({
+            _id: user.id,
+            name: user.name,
+            email: user.email,
+            password: user.password
+        })
+
+    } else {
+        res.status(400)
+        throw new Error('Invalid User data')
+    }
+
+    res.status(200).json({message: 'Register User'});
+})
+
+const loginUser = asyncHandler(async (req, res) => {
     res.status(200).json({message: 'Login User'})
-}
+})
 
-const getMe = (req, res) => {
+const getMe = asyncHandler(async (req, res) => {
     res.status(200).json({message: 'Get Me'})
-}
+})
 
 module.exports = {
     registerUser,
